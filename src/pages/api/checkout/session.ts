4b9d37892090ffecd02763ca33a2c16f619bc26c
Ahmad <hereahmad@gmail.com>
2022-12-14 21:27:42 +0300

    Console log

--- src/pages/api/checkout/session.ts
+++ src/pages/api/checkout/session.ts
@@ -34,8 +34,46 @@ export default async function handler(
 
     return res.redirect(303, session.url!);
   } catch (err: any) {
+    console.log('CATCH')
     return res.status(400).json(err.message);
   }
-
+  console.log('JUST ERROR')
   return res.status(400).json({});
 }
+
+// Backup code
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const session = await stripe.checkout.sessions.create({
+//       allow_promotion_codes: true,
+//       metadata: {
+//         projectId: req.query.ppi as string,
+//       },
+//       line_items: [
+//         {
+//           price_data: {
+//             currency: "usd",
+//             unit_amount: Number(process.env.NEXT_PUBLIC_STRIPE_STUDIO_PRICE),
+//             product_data: {
+//               name: "Studio model training + 100 shots",
+//             },
+//           },
+//           quantity: 1,
+//         },
+//       ],
+//       mode: "payment",
+//       success_url: `${process.env.NEXTAUTH_URL}/dashboard?session_id={CHECKOUT_SESSION_ID}&ppi=${req.query.ppi}`,
+//       cancel_url: `${process.env.NEXTAUTH_URL}/dashboard`,
+//     });
+
+//     return res.redirect(303, session.url!);
+//   } catch (err: any) {
+//     return res.status(400).json(err.message);
+//   }
+
+//   return res.status(400).json({});
+// }
