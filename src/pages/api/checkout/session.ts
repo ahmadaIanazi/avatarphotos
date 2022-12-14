e71bd01acc11853beafa0680d33b9131066eef53
Ahmad <hereahmad@gmail.com>
2022-12-14 21:16:43 +0300

    fixed currency

--- src/pages/api/checkout/session.ts
+++ src/pages/api/checkout/session.ts
@@ -18,7 +18,7 @@ export default async function handler(
       line_items: [
         {
           price_data: {
-            currency: "aed",
+            currency: "usd",
             unit_amount: Number(process.env.NEXT_PUBLIC_STRIPE_STUDIO_PRICE),
             product_data: {
               name: "Studio model training + 100 shots",
